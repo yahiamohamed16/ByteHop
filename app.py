3300e0cd99e772675858b05d22392042be0f00a5
Yahia Mohamed <yahia.moh2amed@gmail.com>
2025-12-31 01:06:03 +0200

    Prepare for Railway deployment

--- app.py
+++ app.py
@@ -283,4 +283,7 @@ with app.app_context():
     db.session.commit()
     print("Database & subjects ready!")
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
