ceb3147468f5520eaf82da4868bc21393d7dde9a
Yahia Mohamed <yahia.moh2amed@gmail.com>
2026-01-30 22:25:31 +0200

    اضافه الدرس الشروط

--- add_lesson.py
+++ add_lesson.py
@@ -1,14 +1,14 @@
 from app import app, db, Lesson
 
 with app.app_context():  # هنا نفتح الـ context
-    lesson37 = Lesson(
-        title="Chapter 18 Qureo: دليلك لإنشاء موقع ويب للصف الأول الثانوي",
-        description="في هذا الفيديو من Chapter 18 على Qureo، ستتعلم أساسيات HTML لبناء موقع إلكتروني: الوسوم الأساسية، القوائم، وعرض الصور بطريقة سهلة مع أمثلة عملية واقعية.",
-        video_url="https://www.youtube.com/watch?v=IkzFl60Jac0",
-        quiz_url="https://docs.google.com/forms/d/e/1FAIpQLSddwhwt5PnLAzhV1x0d0huB447727W7Zl-GmIzDY6powfnLhg/viewform",
+    lesson38 = Lesson(
+        title="شرح Chapter 21 | الشروط (Conditions)",
+        description="1",
+        video_url="https://www.youtube.com/watch?v=bxptmGCAJJI",
+        quiz_url="https://docs.google.com/forms/d/e/1FAIpQLSfp0yTPx-IbEbsALgvDwbL3IhkT8DOHEwo_6raImP5bWvvYzg/viewform?usp=publish-editor",
         subject_id=2
     )
     
-    db.session.add(lesson37)
+    db.session.add(lesson38)
     db.session.commit()
     print("Lesson added successfully!")
