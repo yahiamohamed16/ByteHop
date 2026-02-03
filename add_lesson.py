97d0e0d11ca38dde5e2b700497d594e52a765bef
Yahia Mohamed <yahia.moh2amed@gmail.com>
2026-02-03 21:12:17 +0200

    Added Chapter 22 lesson for ByteHop

--- add_lesson.py
+++ add_lesson.py
@@ -1,14 +1,14 @@
 from app import app, db, Lesson
 
 with app.app_context():  # هنا نفتح الـ context
-    lesson38 = Lesson(
-        title="شرح Chapter 21 | الشروط (Conditions)",
+    lesson39 = Lesson(
+        title="شرح for loop في JavaScript ببساطة | Chapter 22",
         description="1",
-        video_url="https://www.youtube.com/watch?v=bxptmGCAJJI",
-        quiz_url="https://docs.google.com/forms/d/e/1FAIpQLSfp0yTPx-IbEbsALgvDwbL3IhkT8DOHEwo_6raImP5bWvvYzg/viewform?usp=publish-editor",
+        video_url="https://www.youtube.com/watch?v=63UqxaAWmtg",
+        quiz_url="https://docs.google.com/forms/d/e/1FAIpQLSdOSdAu1xM4jdGumsleHHlliABkwxqlKD9SZnl1YrRJXR1Xtg/viewform?usp=dialog",
         subject_id=2
     )
     
-    db.session.add(lesson38)
+    db.session.add(lesson39)
     db.session.commit()
     print("Lesson added successfully!")
